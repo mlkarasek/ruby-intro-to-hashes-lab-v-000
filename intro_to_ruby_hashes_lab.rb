82bdebb4cbbca95d68ec4560b55da20d1755ecdd
Monica  Karasek <mlkarasek@gmail.com>
2017-10-21 21:54:40 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,15 +11,13 @@ def monopoly
 	monopoly = {
 		:railroads => {}
 	}
-
-
 end
 
 def monopoly_with_second_tier
 	monopoly = {
 		:railroads => {:pieces => 4, :names =>{}, :rent_in_dollars => {}
 	}
-
+}
 end
 
 def monopoly_with_third_tier
